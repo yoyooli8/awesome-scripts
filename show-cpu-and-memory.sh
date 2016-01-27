cc822e8e42a19603aa655a3445e9fbbbd1b1fe72
superhj1987 <superhj1987@126.com>
2016-01-27 18:07:11 +0800

    add x to chech-vm.py

--- show-cpu-and-memory.sh
+++ show-cpu-and-memory.sh
@@ -14,10 +14,15 @@ readonly total_cpu="`top -n 1 | grep 'Cpu'`"
 
 echo '**********'$cur_date'**********'
 echo
-echo "total memory: $total_mem total cpu: $total_cpu"
+echo "total memory: $total_mem\n total cpu: $total_cpu"
 echo
 
 for pid in `ps -ef | awk 'NR > 0 {print $2}'` ; do
+	# not pid
+ 	if [[ $pid == *[!0-9]* ]]; then
+ 		continue
+ 	fi
+
     mem=`cat /proc/$pid/status 2> /dev/null | grep VmRSS | awk '{print $2 $3}'`
     cpu=`top -n 1 -b | awk -v "pid=${pid}" '$1==pid {print $9}'`
 
